6c38e1f9f224a0d908d4f00c1428004efce86daa
AL17F13 <asa.benitez23@gmail.com>
2015-04-28 13:24:05 -0500

    dos
    segunda subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,12 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+$ ('#btnvibrar') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('btnbeep') .on ('tap' , function () {
+	 navigator.beep (1) ;
+ }); // tap de beep 
 }); 
 });
 
